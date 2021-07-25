840c7f1530d21dd6722491f54893b3edc181679f
unknown <bmvantunes@outlook.com>
2021-07-25 18:38:02 +0100

    after example number 1

--- src/examples/example1/randomBetween.spec.tsx
+++ src/examples/example1/randomBetween.spec.tsx
@@ -1,9 +1,38 @@
 import { randomBetween } from './randomBetween';
 
+const randomSpy = jest.spyOn(Math, 'random');
+
 describe('randomBetween', () => {
   describe('when Math.random() returns 0', () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockReturnValue(0);
+    });
+
     it('called with min=3 and max=5 returns 3', () => {
-      expect(randomBetween(3, 5)).toBeGreaterThanOrEqual(3);
+      expect(randomBetween(3, 5)).toBe(3);
+      expect(Math.random).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('when Math.random() returns 0.5', () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockReturnValue(0.5);
+    });
+
+    it('called with min=3 and max=5 returns 4', () => {
+      expect(randomBetween(3, 5)).toBe(4);
+      expect(Math.random).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('when Math.random() returns 0.999999', () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockImplementation(() => 0.999999);
+    });
+
+    it('called with min=3 and max=5 returns 5', () => {
+      expect(randomBetween(3, 5)).toBe(5);
+      expect(Math.random).toHaveBeenCalledTimes(1);
     });
   });
 });
